84429ace892329bc99e37ed572938a604bdfd08d
caterada <caterada@gmail.com>
2022-09-07 11:16:58 -0700

    add html in rules and simple css. partial psuedo-code in script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 /*----- constants -----*/
+//let correctLettersGuessed = 0
+//let incorrectLettersGuessed = 0
+
 /*----- app's state (variables) -----*/
+//each letter
+
 /*----- cached element references -----*/
+//correctLettersGuessed
+//incorrectLettersGuessed
+
 /*----- event listeners -----*/
+//when Play Game button is clicked, go to rules.html page
+//when Start button is clicked, go to game.html page
+//when letter is pressed, run function
+//when Restart button is clicked, refresh cache
+
 /*----- functions -----*/
+//check if letter is correct or incorrect
+//if correct, change letter-button to green, run function for putting letter in correct blank spot
+//if incorrect, change letter-button to red, rn function to move cat up 1 flight of stairs (change image)
+//if all blanks are filled, set a timer of 1sec and change page to win.html
+//if incorrect letters == 6, set a timer of 1sec and change page to lose.html
