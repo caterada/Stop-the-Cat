9c1b1008022ba32e4eed2157dae03eb21a64b48e
caterada <caterada@gmail.com>
2022-09-08 15:27:29 -0700

    add counter for incorrect guesses and function to show correct letter, function to show correct image if guess is incorrect. add win & lose html and js files

--- js/game.js
+++ js/game.js
@@ -4,13 +4,22 @@
 const restartButton = document.querySelector('#restart-button');
 //each letter
 
-const wordList = ['apple', 'forgot', 'listen'];
+const wordList = ['apples', 'forgot', 'listen'];
 
-const imageCat = document.querySelector('.image-cat');
+const imageCat = document.querySelector('#image-cat-counter');
+
+const letterOne = document.querySelector('#letter-01');
+const letterTwo = document.querySelector('#letter-02');
+const letterThree = document.querySelector('#letter-03');
+const letterFour = document.querySelector('#letter-04');
+const letterFive = document.querySelector('#letter-05');
+const letterSix = document.querySelector('#letter-06');
 
 //keyboard values
 let keyValues = 'qwertyuiopasdfghjklzxcvbnm';
 let arrayOfKeys = keyValues.split('');
+
+let counter = 0;
 
 for (let i = 0; i < arrayOfKeys.length; i++) {
 	//create keyboard button with ID of A and innertext of A
@@ -70,10 +79,6 @@ let CorrectWordObject = new CorrectWordClass(
 	CorrectWordArray[6]
 );
 
-// CONSOLE LOG CHECKS
-console.log(wordList);
-console.log(CorrectWordObject);
-
 /*----- cached element references -----*/
 //correctLettersGuessed
 //incorrectLettersGuessed
@@ -85,20 +90,11 @@ restartButton.addEventListener('click', restartGame);
 
 /*----- functions -----*/
 
-//when letter is clicked, check to see if letter is in the randomCorrectWord
-//if it's a correct letter
-
-//else if it's not a correct letter function needed
-
 //if correct, change letter-button to green, run function for putting letter in correct blank spot
-// function guessedWord(letter) {
-// 	let updateLetter = document.querySelector('#letter-01').innerHTML;
-// 	updateLetter = CorrectWordArray[randomCorrectWord.indexOf(letter)];
-// }
 
 //if incorrect, change letter-button to red, run function to move cat up 1 flight of stairs (change image)
 //if all blanks are filled, set a timer of 1sec and change page to win.html
-//if incorrect letters == 6, set a timer of 1sec and change page to lose.html
+
 function restartGame() {
 	//enter function code here to reset all the values
 	location.reload();
@@ -111,12 +107,82 @@ function pickRandomWord() {
 
 //if button ID = a letter in the array of correct word, replace innertext with that letter. if not, move the incorrect counter up (image changes)
 function clickButton() {
-	// let CorrectWordArray = randomCorrectWord.split('');
-	// let keyEle = document.getElementById(i)
-	if (CorrectWordArray.includes(this.innerText)) {
-		//replace 1 - 6 with innerText that matches
+	if (randomCorrectWord.includes(this.innerText)) {
+		//make for loop later
+		if (CorrectWordArray[0] === this.innerText) {
+			//replace 1 - 6 with innerText that matches
+			letterOne.innerHTML = this.innerText;
+		}
+		if (CorrectWordArray[1] === this.innerText) {
+			//replace 1 - 6 with innerText that matches
+			letterTwo.innerHTML = this.innerText;
+		}
+		if (CorrectWordArray[2] === this.innerText) {
+			//replace 1 - 6 with innerText that matches
+			letterThree.innerHTML = this.innerText;
+		}
+		if (CorrectWordArray[3] === this.innerText) {
+			//replace 1 - 6 with innerText that matches
+			letterFour.innerHTML = this.innerText;
+		}
+		if (CorrectWordArray[4] === this.innerText) {
+			//replace 1 - 6 with innerText that matches
+			letterFive.innerHTML = this.innerText;
+		}
+		if (CorrectWordArray[5] === this.innerText) {
+			//replace 1 - 6 with innerText that matches
+			letterSix.innerHTML = this.innerText;
+		}
+	} else {
+		counter += 1;
+		counterAdd();
+		if (counter === 6) {
+			setTimeout(displayLosePage, '6000');
+		}
 	}
 }
 
-// function guessLetter () {
-// }
+function counterAdd() {
+	if (counter === 1) {
+		imageCat.src = 'images/cat-image01.jpg';
+		imageCat.alt = 'cat climbed up 1st flight of stairs';
+	}
+
+	if (counter === 2) {
+		imageCat.src = 'images/cat-image02.jpg';
+		imageCat.alt = 'cat climbed up 2nd flight of stairs';
+	}
+
+	if (counter === 3) {
+		imageCat.src = 'images/cat-image03.jpg';
+		imageCat.alt = 'cat climbed up 3rd flight of stairs';
+	}
+
+	if (counter === 4) {
+		imageCat.src = 'images/cat-image04.jpg';
+		imageCat.alt = 'cat climbed up 4th flight of stairs';
+	}
+
+	if (counter === 5) {
+		imageCat.src = 'images/cat-image05.jpg';
+		imageCat.alt = 'cat climbed up 5th flight of stairs';
+	}
+
+	if (counter === 6) {
+		imageCat.src = 'images/cat-image06.jpg';
+		imageCat.alt = 'cat climbed up 6th flight of stairs';
+	}
+}
+
+function displayLosePage() {
+	document.location.href = '/lose.html';
+}
+
+function displayWinPage() {
+	document.location.href = '/win.html';
+}
+
+// CONSOLE LOG CHECKS
+console.log(wordList);
+console.log(CorrectWordObject);
+console.log(counter);
